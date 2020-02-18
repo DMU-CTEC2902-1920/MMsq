75cf5b704840d716d8e95a5275c62777319c9558
P2421782 <P2421782@my365.dmu.ac.uk>
2020-02-18 18:54:38 +0000

    Changed Index.cshtml by adding MMsq header and a text about the group.

---
+++ MMsqGameReview/MMsqGameReview/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MMsqGameReview.Startup))]
+namespace MMsqGameReview
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
