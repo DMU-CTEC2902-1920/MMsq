3a9453e104cac62e1e1973f95db2d331a949005c
Sufiyaan Hussain <P2421782@my365.dmu.ac.uk>
2020-03-15 20:38:17 +0000

    Modified clsReview and clsUser. Added User Controller and View. Set up User Details page.

---
+++ MMSQ/MMSQ/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MMSQ.Models;
+
+namespace MMSQ.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View(CreateUser());
+        }
+
+        private clsUser CreateUser()
+        {
+            clsUser NewUser = new clsUser();
+            NewUser.UserID = 1;
+            NewUser.User_Username = "BestGamer";
+            NewUser.User_Password = "Password123";
+            NewUser.User_Firstname = "Paul";
+            NewUser.User_Lastname = "Pogba";
+            NewUser.User_Telephone = "123456789123";
+            NewUser.User_Email = "PP6@gmail.com";
+            NewUser.User_DateOfBirth = Convert.ToDateTime("01/01/2020");
+            NewUser.User_AccountStatus = true;
+
+            NewUser.GameReview = new List<clsReview>()
+            {
+                new clsReview {ReviewID = 1, Review_GameTitle = "FIFA 20", Review_ReviewDescription = "Not better than FIFA 19!!", Review_ReviewStatus = true },
+                new clsReview {ReviewID = 2, Review_GameTitle = "Call Of Duty: Modern Warfare", Review_ReviewDescription = "SOOOO Epic. Great game!!", Review_ReviewStatus = true},
+                new clsReview {ReviewID = 3, Review_GameTitle = "Tom Clancy: Rainbow Six Siege", Review_ReviewDescription = "was an improvement from the last game", Review_ReviewStatus = true}
+            };
+            return NewUser;
+        }
+    }
+}
